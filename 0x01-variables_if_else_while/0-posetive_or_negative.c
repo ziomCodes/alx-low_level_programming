54c65dc30163b8e55f714b70c96d7c29b10b2a28
ziomCodes <ziomcode@gmail.com>
2022-12-09 12:36:02 +0000

    This is task 0 commit

--- 0x01-variables_if_else_while/0-posetive_or_negative.c
+++ 0x01-variables_if_else_while/0-posetive_or_negative.c
@@ -2,7 +2,12 @@
 #include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
-/* main - main block Description: Get a random number and print the number and if it is positive, negative, or zero */
+/**
+ * main - main block
+ * Description: Get a random number and print the number
+ * and if it is positive, negative, or zero
+ * Return: 0
+ */
 int main(void)
 {
 	int n;
