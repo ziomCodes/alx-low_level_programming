4e083664f644f0013194e8c8f84a8e16e3ebbeff
ziomCodes <ziomcode@gmail.com>
2022-12-09 19:41:12 +0000

    This is task 3 commit

---
+++ 0x01-variables_if_else_while/3-print_alphabet.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Use `putchar` function to print the alphabet
+ * in lowercase and uppercase
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	c = 'A';
+
+	while (c <= 'Z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+
+	return (0);
+}
