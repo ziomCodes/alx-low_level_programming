d1d966629cac8c4b9942defa304aa682d72392ba
ziomCodes <ziomcode@gmail.com>
2022-12-09 18:57:45 +0000

    This is task 1 commit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <studio.h>
+
+/**
+ * main - main block
+ * Description: Print statements based on the last digit of the random number.
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	int m = n % 10;
+
+	printf("Last digit of %d is %d ", n, m));
+	if (m  > 5)
+	{
+	printf("and is greater than 5\n");
+	}
+	else if (m == 0)
+	{
+	printf("and is 0\n");
+	}
+	else if (m < 6 && m != 0)
+	{
+	printf("and is less than 6 and not 0\n");
+	}
+	return (0);
+}
