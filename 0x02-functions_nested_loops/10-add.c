121f133e32cc8bc6d5fa0fc0de95b730aeb1dfd4
ziomCodes <ziomcode@gmail.com>
2022-12-13 20:07:29 +0000

    This is task 10 commit

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,15 +1,13 @@
 #include "main.h"
 /**
- *add - adds two numbers
- *Description: adds two numbers
- *@num1: type integer
- *@num2: type integer
- *Return: return the sum
+ * add - adds two numbers
+ * Description: adds two numbers
+ * @num1: type integer
+ * @num2: type integer
+ * Return: return the sum
  */
-int add(int num1 int num2)
+int add(int num1, int num2)
 {
-	int num1;
-	int num2;
 	int sum;
 
 	sum = num1 + num2;
