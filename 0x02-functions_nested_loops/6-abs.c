bf7e4582beb851b5bf8c757faba94d58227b7222
ziomCodes <ziomcode@gmail.com>
2022-12-13 18:12:03 +0000

    This is task 6 commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* _abs - prints the absolute value of a number
+*@i: number to be printed
+*Return: returns absolute value of @i
+*/
+int _abs(int i)
+{
+	if (i > 0)
+	{
+	return (i);
+	}
+	else if (i < 0)
+	{
+	return (-i);
+	}
+}
