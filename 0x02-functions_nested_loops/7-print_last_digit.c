8ca42adc2cbd911bb5c5dc27be22c5bb45de3e09
ziomCodes <ziomcode@gmail.com>
2022-12-13 18:42:33 +0000

    This is task 7 commit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,7 +2,7 @@
 /**
 *print_last_digit -  print last digit of a number
 *@n: number to be tried
-*Return: returns 0
+*Return: returns return value of last digit
 */
 int print_last_digit(int n)
 {
@@ -11,11 +11,10 @@ int print_last_digit(int n)
 	if (last_digit < 0)
 		{
 		last_digit = last_digit * -1;
-		_putchar(last_digit + '0');
+		_putchar('0' + last_digit);
 		}
 	else
 		{
-		_putchar(last_digit + '0');
+		_putchar('0' + last_digit);
 		}
-	return (0);
 }
