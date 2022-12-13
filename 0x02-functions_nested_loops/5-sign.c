32d7427aec76bdca95d99699d83be134a528e634
ziomCodes <ziomcode@gmail.com>
2022-12-13 17:57:33 +0000

    This is task 5 commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - Print the sign of a number
+ * @n: type int integer
+ * Description: print +, 0, or - depending on number, along with return
+ * Return: 1 if +, 0 if 0, and -1 if -
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar(-49);
+		return (-1);
+	}
+}
