a8867108115329ca0107120579ec70a99eb18a02
ziomCodes <ziomcode@gmail.com>
2022-12-15 20:39:49 +0000

    This is task 9 commit

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - main block doc
+ * Description: Multiples of 3, print Fizz. Multiples of 5, print Buzz.
+ * Multiples of both 3 and 5 should print FizzBuzz.
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	n = 1;
+	while (n <= 100)
+	{
+		if (n % 3 == 0 && n % 5 == 0)
+			printf("FizzBuzz");
+		else if (n % 3 == 0)
+			printf("Fizz");
+		else if (n % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", n);
+
+		if (n != 100)
+			printf(" ");
+		n++;
+	}
+	printf("\n");
+
+	return (0);
+}
