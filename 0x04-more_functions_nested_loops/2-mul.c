c64552a7fa1e774e4ecbe3f34a8e67f35b063f32
ziomCodes <ziomcode@gmail.com>
2022-12-15 19:43:32 +0000

    This is task 2 commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - functioning
+ * Description: multiplies two numbers
+ * @a: type integer
+ * @b: type integer
+ * Return: return the multiply
+ */
+int mul(int a, int b)
+{
+	int ans;
+
+	ans  = a * b;
+	return (ans);
+}
