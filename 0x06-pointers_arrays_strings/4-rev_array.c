82b69dca286df09fa892e8fea18c39d1063b9d7a
ziomCodes <ziomcode@gmail.com>
2022-12-22 09:11:52 +0000

    This is task 4 commit

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse the elements of an array integers
+ * @a: array of integers
+ * @n: number of elements in array
+ */
+
+void reverse_array(int *a, int n)
+{
+	int num;
+	int num1;
+
+	for (num = 0; num < (n / 2); num++)
+	{
+		num1 = a[num];
+		a[num] = a[n - num - 1];
+		num1 = a[n - num - 1];
+	}
+}
