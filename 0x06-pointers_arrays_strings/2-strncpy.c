a2df4ba5d447446e024ceecf7ae6599417f1fd69
ziomCodes <ziomcode@gmail.com>
2022-12-22 08:44:24 +0000

    This is task 2 commit

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * _strncpy - Copy a string
+ * @dest: string destination
+ * @src: string to copy
+ * @n: number of characters to copy
+ * Return: string 'dest'
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int num;
+
+	for (num = 0; num < n && src[num] != '\0'; num++)
+		dest[num] = src[num];
+
+	for (; n > num; num++)
+		dest[num] = '\0';
+
+	return (dest);
+}
