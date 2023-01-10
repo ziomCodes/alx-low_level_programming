dc01218446c8b0e343de75239896e67da05a481e
ziomCodes <ziomcode@gmail.com>
2023-01-10 10:04:44 +0000

    This are commit messages for the memory allocation task projects

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,3 +1,5 @@
+#ifndef MAIN_H
+#define MAIN_H
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
@@ -6,3 +8,6 @@ int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
 char **strtow(char *str);
+int len(char *str);
+int numWord(char *str);
+#endif
