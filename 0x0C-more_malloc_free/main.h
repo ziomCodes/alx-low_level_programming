f78d159c7df8a9d564c78c326d4c26ca2f8ea25f
ziomCodes <ziomcode@gmail.com>
2023-01-12 19:59:45 +0000

    This are task commits for the more malloc projects

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -10,11 +10,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-int find_len(char *str);
-char *create_xarray(int size);
-char *iterate_zeroes(char *str);
-int get_digit(char c);
-void get_prod(char *prod, char *mult, int digit, int zeroes);
-void add_nums(char *final_prod, char *next_prod, int next_len);
+unsigned int *initDigitArray(size_t size);
+void stringIntMultiply(unsigned int *prod_digits, char *n1_digits,
+		       char *n2_digits, size_t n1_len, size_t n2_len);
+int stringIsPosInt(char *s);
+void error(int status);
 
 #endif
