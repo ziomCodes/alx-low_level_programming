db436bfb6060c306001ecc78aa4f92502bdf1c09
ziomCodes <ziomcode@gmail.com>
2023-01-23 07:26:21 +0000

    This is task 1 commit

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - execute a function pointer on each element of an array
+ * @array: array of integers
+ * @size: size of array
+ * @action: function pointer
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (array == NULL || action == NULL)
+	return;
+
+	while (size-- > 0)
+	{
+	action(*array);
+	array++;
+	}
+
+}
