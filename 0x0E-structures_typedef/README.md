5832581d28f5ade2f8376498cc77f424e7b5556d
ziomCodes <ziomcode@gmail.com>
2023-01-23 19:28:10 +0000

    This is task 0 commit

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This are task projects of Structures and typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure definition of a dog
+ * @name: character string
+ * @age: integer
+ * @owner: character string
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
