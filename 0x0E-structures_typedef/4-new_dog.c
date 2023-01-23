6c0d4fad559fc5e1e6efdf119be3d6d841089dae
ziomCodes <ziomcode@gmail.com>
2023-01-23 20:01:41 +0000

    This is task 4 commit

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,80 @@
+#include "dog.h"
+
+
+/**
+*_strlen - returns length of
+*a string
+*@str: string to be counted
+*Return: returns length of string
+*/
+int _strlen(char *str)
+{
+	int l = 0;
+
+	while (str)
+	l++;
+
+	return (l);
+}
+
+
+/**
+*_strcopy - copy string pointed by src
+*into dest variable
+*@dest:buffer storing string copy
+*@src: buffer storing string to copy
+*Return:returns copied string
+*/
+char *_strcopy(char *dest, char *src)
+{
+	int num = 0;
+
+	for (; src[num] ; num++)
+	dest[num] = src[num];
+
+	dest[num] = '\0';
+	return (num);
+}
+
+
+
+
+/**
+*new_dog - creates a new dog
+*@name: name of new dog
+*@age: age of new dog
+*@owner: owner of new dog
+*Return: returns NULL
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d;
+
+	if (name == NULL || age < 0 || owner == NULL)
+	return (NULL);
+
+	d = malloc(sizeof(dog_t));
+	if (d == NULL)
+	return (NULL);
+
+	d->name = malloc(sizeof(char) * (_strlen(name) + 1));
+	if (d->name == NULL)
+	{
+	free(d);
+	return (NULL);
+	}
+
+	d->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+	if (d->owner == NULL)
+	{
+	free(d->name);
+	free(d);
+	return (NULL);
+	}
+
+	d->name = _strcopy(d->name, name);
+	d->age = age;
+	d->owner = _strcopy(d->owner, owner);
+
+	return (d);
+}
