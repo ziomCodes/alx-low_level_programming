9e12b9f96345051135595c0cd0c842f206ef8da5
ziomCodes <ziomcode@gmail.com>
2023-01-23 19:31:23 +0000

    This is task 0 commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct dog - structure definition of a dog
  * @name: character string
  * @age: integer
  * @owner: character string
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
