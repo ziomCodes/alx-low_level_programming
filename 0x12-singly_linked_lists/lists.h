f64d698d256d4fcd2a760a4715878dde79250b8c
ziomCodes <ziomcode@gmail.com>
2023-01-26 19:13:05 +0000

    this is task 0 commit

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,7 +3,6 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
 
 /**
  * struct list_s - singly linked list
@@ -15,12 +14,13 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+char *str;
+unsigned int len;
+struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
 int _putchar(char c);
 
 #endif
