bb654a4c14e43f237e175ffe9de9655d9d3bd496
ziomCodes <ziomcode@gmail.com>
2023-01-26 12:43:10 +0000

    This is task 0 commit

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -7,7 +7,7 @@
 
 /**
  * struct list_s - singly linked list
- * @str: string
+ * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
  *
@@ -15,11 +15,12 @@
  */
 typedef struct list_s
 {
-	char *str;
-	unsigned int len;
-	struct list_s *next;
+    char *str;
+    unsigned int len;
+    struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
+int _putchar(char c);
 
 #endif
