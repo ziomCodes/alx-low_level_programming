7f888c459018aecde6512b1c110f4a9f96160ba0
ziomCodes <ziomcode@gmail.com>
2023-02-14 12:53:15 +0000

    This is task 5 commit

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+*free_listint2 - frees a linked list
+*@head: pointer to the head of the list
+*
+*Return: void
+*/
+
+void free_listint2(listint_t **head)
+{
+	listint_t *pointt;
+	listint_t **tbf = head;
+
+	if (tbf != NULL)
+	{
+	while (*head != NULL)
+	{
+	pointt = *head;
+	free(pointt);
+	*head = (*head)->next;
+	}
+
+	*tbf = NULL;
+
+}
+}
