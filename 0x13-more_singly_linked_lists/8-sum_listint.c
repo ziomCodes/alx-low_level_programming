4dafd64fd69c901cbc1f655c7b2b2cf3bcf96ba5
ziomCodes <ziomcode@gmail.com>
2023-02-16 09:30:04 +0000

    This is the 8 commit

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ *sum_listint - a function that returns the sum of all the data (n) of
+ *a linked list.
+ *@head: a pointer that points to the address of the struct
+ *Return: The total of the integers added together
+ */
+int sum_listint(listint_t *head)
+{
+	int num;
+
+	if (head == NULL)
+		return (0);
+	num = 0;
+	while (head != NULL)
+	{
+		num += head->num;
+		head = head->next;
+	}
+	return (num);
+}
