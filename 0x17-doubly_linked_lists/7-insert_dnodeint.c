1a10280e674294d9b184c6b9ccfda2daffac7892
ziomCodes <ziomcode@gmail.com>
2023-03-16 09:55:17 +0000

    task 7 commit

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,52 +1,46 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_idx - insert a new node at given position
- * @h: double pointer to head
- * @idx: index to insert into
- * @n: value to store in new node
- * Return: Address of new node, or NULL if failed
+ * insert_dnodeint_at_index - Inserts a new node at a given \
+ * position in a dlistint_t list
+ * @h: The pointer to the head node's address
+ * @idx: The position at which to add the new node
+ * @n: The item to store in the new node
+ *
+ * Return: A pointer to the new node or NULL if it failed
  */
-dlistint_t *insert_dnodeint_at_idx(dlistint_t **h, unsigned int idx, int n)
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int c;
-	dlistint_t *tmp, *prev, *new;
+	dlistint_t *new_node = NULL, *node = NULL;
+	unsigned int i = 0;
 
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = n;
-	for (tmp = *h, c = 1; tmp && c < idx; c++, tmp = tmp->next)
-		prev = tmp;
-	if (idx == 0)
+	if (h != NULL)
 	{
-		*h = new; new->prev = NULL;
-		new->next = (tmp == NULL) ? NULL : tmp;
-		return (new);
-	}
-	if (idx == 1)
-	{
-		prev = *h;
-		tmp = ((*h)->next == NULL) ? NULL : (*h)->next;
-		new->prev = prev; new->next = tmp; prev->next = new;
-		if (tmp)
-			tmp->prev = new;
-		return (new);
-	}
-	if (idx == c && tmp == NULL)
-	{
-		if (prev != NULL)
+		node = *h;
+		while ((i != idx) && (node != NULL))
 		{
-			new->prev = prev; new->next = NULL;
-			prev->next = new; return (new);
+			node = node->next;
+			i++;
 		}
-		free(new); return (NULL);
+		if (i == idx)
+		{
+			if (node == NULL)
+				return (add_dnodeint_end(h, n));
+			new_node = malloc(sizeof(dlistint_t));
+			if (new_node != NULL)
+			{
+				new_node->n = n;
+				new_node->next = node, new_node->prev = NULL;
+				if (node->prev != NULL)
+				{
+					node->prev->next = new_node;
+					new_node->prev = node->prev;
+				}
+				node->prev = new_node;
+				if (i == 0)
+					*h = new_node;
+			}
+		}
 	}
-	else if (idx != c && tmp == NULL)
-	{
-		free(new); return (NULL);
-	}
-	prev = tmp; tmp = tmp->next; new->prev = prev;
-	new->next = tmp; prev->next = new; tmp->prev = new;
-	return (new);
+	return (new_node);
 }
