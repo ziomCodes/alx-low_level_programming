ec2dc494f9d04a962ae4f335690a2451389e303f
ziomCodes <ziomcode@gmail.com>
2023-04-07 08:05:01 +0000

    hash update

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -2,26 +2,25 @@
 
 /**
  * hash_table_create - Creates a hash table.
- * @size: The size of the array.
+ * @size: The number of top-level items in the hash table.
  *
- * Return: If an error occurs - NULL.
- *         Otherwise - a pointer to the new hash table.
+ * Return: A pointer to the created hash table, otherwise NULL.
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ht;
+	hash_table_t *tbl = NULL;
 	unsigned long int i;
 
-	ht = malloc(sizeof(hash_table_t));
-	if (ht == NULL)
-		return (NULL);
-
-	ht->size = size;
-	ht->array = malloc(sizeof(hash_node_t *) * size);
-	if (ht->array == NULL)
-		return (NULL);
-	for (i = 0; i < size; i++)
-		ht->array[i] = NULL;
-
-	return (ht);
+	tbl = malloc(sizeof(hash_table_t));
+	if (tbl != NULL)
+	{
+		tbl->size = size;
+		tbl->array = malloc(sizeof(hash_node_t *) * size);
+		if (tbl->array != NULL)
+		{
+			for (i = 0; i < size; i++)
+				tbl->array[i] = NULL;
+		}
+	}
+	return (tbl);
 }
